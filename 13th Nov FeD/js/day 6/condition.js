3f26e833bc6723bae85c461ea9b39c91fec63f65
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-06-20 14:47:58 +0530

    comits

--- 13th Nov FeD/js/day 6/condition.js
+++ 13th Nov FeD/js/day 6/condition.js
@@ -1,47 +1,62 @@
-if else and switch 
+// if else and switch 
 
 
-let age = 25;
+// let age = 25;
 
-if (age >= 18) {
-    console.log("You are an Adult ");
-} else {
-    console.log("Yor are Not a Adult");
-}
+// if (age >= 18) {
+//     console.log("You are an Adult ");
+// } else {
+//     console.log("Yor are Not a Adult");
+// }
 
-switch (expression) {
-  case x:
-    // code ex block
-    break;
-  case y:
-    // code ex  block
-    break;
-  default:
-  // code ex block
-}
+// switch (expression) {
+//   case x:
+//     // code ex block
+//     break;
+//   case y:
+//     // code ex  block
+//     break;
+//   default:
+//   // code ex block
+// }
 
 
 
-let day = 4;
+// let day = 4;
 
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wednesday");
+//     break;
+//   case 4:
+//     console.log("thursday");
+//     break;
+//     default:
+//         console.log("Looking forward to the Weekend");
+// }
+
+
+
+
+
+
+
+const day = 4 
 switch (day) {
-  case 1:
-    console.log("Monday");
-    break;
+  case 1: 
+  break;
   case 2:
-    console.log("tuesday");
     break;
-  case 3:
-    console.log("wednesday");
-    break;
-  case 4:
-    console.log("thursday");
-    break;
-    default:
-        console.log("Looking forward to the Weekend");
+    case 4:
+      break;
+      default: 
+      console.log("you are brilliant sowie");
+
 }
-
-
-
-
 
